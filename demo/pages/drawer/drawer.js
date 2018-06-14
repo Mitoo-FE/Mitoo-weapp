fef86171bd58eb399b5718549b44787f9982c7b3
wang125309 <wang_125309@foxmail.com>
2018-06-14 16:08:32 +0800

    fix bug

--- demo/pages/drawer/drawer.js
+++ demo/pages/drawer/drawer.js
@@ -1,2 +1,11 @@
 Page({
+    methods: {
+        open_drawer_left: function() {
+            console.log('I do')
+        },
+        open_drawer_right: function() {
+
+            console.log('I do r')
+        }
+    }
 })
