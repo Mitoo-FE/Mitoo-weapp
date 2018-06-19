d47917db81e9acbeacffb6e4b2c288855b68b698
angel8731 <angel8731@sina.com>
2018-06-19 18:19:12 +0800

    icon

---
+++ src/Icon/index.js
@@ -0,0 +1,17 @@
+Component({
+    externalClasses : ['mit-class'],
+    properties: {
+        type: {
+            type: String
+        },
+        size: {
+            type: Number
+        },
+		color: {
+			type: String
+		}
+    },
+    methods: {
+        
+    }
+})
