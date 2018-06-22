3a79cf57e25e95aad512bc5d5298e893a66bfb76
wang125309 <wang_125309@foxmail.com>
2018-06-22 18:25:20 +0800

    fix badge

--- src/Badge/index.js
+++ src/Badge/index.js
@@ -3,5 +3,17 @@ Component({
         multipleSlots: true
     },
     properties: {
+        number: {
+            type: Number,
+            value: 0
+        },
+        flag: {
+            type: Boolean,
+            value: false
+        },
+        color: {
+            type: String,
+            value: ''
+        }
     }
 })
