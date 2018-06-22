f1c1a2735f46c3def20561e7b340f0bc1bb50f15
wang125309 <wang_125309@foxmail.com>
2018-06-22 16:07:23 +0800

    tag

--- src/Badge/index.js
+++ src/Badge/index.js
@@ -3,17 +3,5 @@ Component({
         multipleSlots: true
     },
     properties: {
-        number: {
-            type: Number,
-            value: 0
-        },
-        flag: {
-            type: Boolean,
-            value: false
-        },
-        color: {
-            type: String,
-            value: ''
-        }
     }
 })
