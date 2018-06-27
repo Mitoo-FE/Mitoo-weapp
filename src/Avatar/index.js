a08d7f081a983828a26605d4f1806a80c8e8d1ca
wang125309 <wang_125309@foxmail.com>
2018-06-27 16:02:35 +0800

    avatar

---
+++ src/Avatar/index.js
@@ -0,0 +1,30 @@
+Component({
+    externalClasses : ['mit-class'],
+    options: {
+        multipleSlots: true
+    },
+    properties: {
+        border: {
+            type: Boolean,
+            value: false
+        },
+        size: {
+            type: String,
+            value: 'default'
+        },
+        src: {
+            type: String,
+            value: ''
+        },
+        noCircle: {
+            type: Boolean,
+            value: false
+        }
+    },
+    data: {
+
+    },
+    methods: {
+
+    }
+})
