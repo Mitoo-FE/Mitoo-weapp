7cc5387016a37488d4b11aece4ab8e1fae10563d
wang125309 <wang_125309@foxmail.com>
2018-06-29 17:23:55 +0800

    be user

--- merge_readme.sh
+++ merge_readme.sh
@@ -1,4 +1,4 @@
 cat init.md > README.md
-echo "\n\n\n\n## 具体组件\n\n\n" >> README.md
+echo "\n\n\n\n## 具体组件\n\n\n\n" >> README.md
 ls src | awk '{print "cat src/" $1"/README.md"}' | sh >> README.md
 cp README.md dist/
