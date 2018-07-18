096545e6fb3412edd8177d3220cb7936b10040bc
wang125309 <wang_125309@foxmail.com>
2018-07-18 17:24:14 +0800

    tabs 添加tab切换回调

--- src/TabsCell/index.js
+++ src/TabsCell/index.js
@@ -1,69 +1,69 @@
 Component({
-    externalClasses : ['mit-class'],
-    properties: {
-        active: {
-            type: Boolean,
-            value: false
-        },
-        activeLeft: {
-            type: Number,
-            value: 0
-        }
+  externalClasses : ['mit-class'],
+  properties: {
+    active: {
+      type: Boolean,
+      value: false
     },
-    data: {
-        brandColor: '',
-        translation: 0,
-        activeLeft: 0
-    },
-    relations: {
-        '../Tabs/index': {
-            type: 'parent',
-            linked(target) {
-                if (target.properties.brandColor) {
-                    this.setData({
-                        'brandColor':target.properties.brandColor
-                    })
-                }
-            }
-        }
-    },
-    ready() {
-        let query = wx.createSelectorQuery().in(this);
-        query.select('.mit-tabs-cell-bottom').boundingClientRect((rect) => {
-            this.setData({
-                'nodeLeft': rect.left
-            });
-        }).exec();
-    },
-    methods: {
-        change_tab(evt) {
-            let parent = this.getRelationNodes('../Tabs/index');
-            if (!parent.length) {
-                console.error('Error tabs cell parent, Tabs-Cell does not has a Tabs parent')
-            }
-            else {
-                let tab = parent[0];
-                let nodes = tab.getRelationNodes('../TabsCell/index');
-                nodes.forEach( (e) => {
-                    if (e.properties.active) {
-                        e.setData({
-                            'active': false
-                        });
-                    }
-                });
-                this.setData({
-                    'active': true
-                });
-                nodes.forEach( (e) => {
-                    e.setData({
-                        'activeLeft': this.data.nodeLeft
-                    });
-                })
-
-            }
-        }
-    },
-    get_active($evt) {
-        console.log($evt)
+    activeLeft: {
+      type: Number,
+      value: 0
     }
+  },
+  data: {
+    brandColor: '',
+    translation: 0,
+    activeLeft: 0
+  },
+  relations: {
+    '../Tabs/index': {
+      type: 'parent',
+      linked(target) {
+        if (target.properties.brandColor) {
+          this.setData({
+            'brandColor':target.properties.brandColor
+          })
+        }
+      }
+    }
+  },
+  ready() {
+    let query = wx.createSelectorQuery().in(this);
+    query.select('.mit-tabs-cell-bottom').boundingClientRect((rect) => {
+      this.setData({
+        'nodeLeft': rect.left
+      });
+    }).exec();
+  },
+  methods: {
+    onChangeTab ($evt) {
+      console.log($evt)
+    },
+    change_tab($evt) {
+      let parent = this.getRelationNodes('../Tabs/index');
+      if (!parent.length) {
+        console.error('Error tabs cell parent, Tabs-Cell does not has a Tabs parent')
+      }
+      else {
+        let tab = parent[0];
+        let nodes = tab.getRelationNodes('../TabsCell/index');
+        nodes.forEach( (e) => {
+          if (e.properties.active) {
+            e.setData({
+              'active': false
+            });
+          }
+        });
+        this.setData({
+          'active': true
+        });
+        nodes.forEach((e) => {
+          e.setData({
+            'activeLeft': this.data.nodeLeft
+          });
+        })
+        this.triggerEvent('onChangeTab', this, {bubbles: true});
+      }
+    }
+  }
 })
