9697cf4a2a07d25cd1e47519a3b876f0e70f208d
wang125309 <wang_125309@foxmail.com>
2018-07-19 10:20:57 +0800

    drawer callback

--- demo/pages/drawer/drawer.js
+++ demo/pages/drawer/drawer.js
@@ -1,14 +1,17 @@
 Page({
-    data: {
-        visible1: false,
-        visible2: false,
-    },
-    open_left: function(evt) {
-        this.data.visible1 = true;
-        this.setData({visible1:this.data.visible1})
-    },
-    open_right: function(evt) {
-        this.data.visible2 = true;
-        this.setData({visible2:this.data.visible2})
-    }
+  data: {
+    visible1: false,
+    visible2: false,
+  },
+  open_left: function($evt) {
+    this.data.visible1 = true;
+    this.setData({visible1:this.data.visible1})
+  },
+  open_right: function($evt) {
+    this.data.visible2 = true;
+    this.setData({visible2:this.data.visible2})
+  },
+  handleHidden: function ($evt) {
+    console.log($evt)
+  }
 })
--- src/Drawer/index.js
+++ src/Drawer/index.js
@@ -1,23 +1,24 @@
 Component({
-    externalClasses : ['mit-class'],
-    options: {
-        multipleSlots: true
+  externalClasses : ['mit-class'],
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    position: {
+      type: String,
+      value: ''
     },
-    properties: {
-        position: {
-            type: String,
-            value: ''
-        },
-        visible: {
-            type: Boolean,
-            value: false
-        }
-    },
-    methods: {
-        drawer_mask_tap: function(event) {
-            this.setData({
-                visible: false
-            })
-        }
+    visible: {
+      type: Boolean,
+      value: false
     }
+  },
+  methods: {
+    drawer_mask_tap: function($evt) {
+      this.setData({
+        visible: false
+      })
+      this.triggerEvent('drawerHidden', $evt);
+    }
+  }
 })
